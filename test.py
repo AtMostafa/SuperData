322421241575786a6b32448c7636695b8dcd698b
mostafa.safaie <mostafa.safaie@gmail.com>
2020-09-20 12:25:42 +0200

    add test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from superdata import *
 
 if __name__ == '__main__':    
-    print(np.array, plt.plot,)
+    print(f'using the _np_ key: {np.array}')
+    print(f'using the _plt_ key: {plt.plot}')

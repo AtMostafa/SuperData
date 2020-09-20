5cc44a4ea7df58347f5271d314a5f136d6b29351
mostafa.safaie <mostafa.safaie@gmail.com>
2020-09-20 12:58:37 +0200

    add more packages

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from superdata import *
 if __name__ == '__main__':    
     print(f'using the _np_ key: {np.array}')
     print(f'using the _plt_ key: {plt.plot}')
+    print(f'using the _pd_ key: {pd}')

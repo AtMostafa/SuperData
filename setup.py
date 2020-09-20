51cf20c9d065c5e1bfd023e1a3b53a7fb1db1538
mostafa.safaie <mostafa.safaie@gmail.com>
2020-09-20 13:06:14 +0200

    update setup

--- setup.py
+++ setup.py
@@ -18,5 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas'
+    ]
 )
